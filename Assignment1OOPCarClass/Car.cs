b438d1eb4e47f75e3928f7e9cb907690ab224c2b
Ugyen Lama <ugyenchheda@gmail.com>
2023-02-09 23:49:44 +0200

    add max speed functionality

---
+++ Assignment1OOPCarClass/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1OOPCarClass
+{
+    class Car
+    {
+        public Car(int yearmodel, string make, String model, int speed) {
+            yearModel = yearmodel;
+            Make = make;
+            Model = model;
+            Speed = speed;
+        }
+        public int yearModel { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Speed { get; set; }
+
+        public int Accelerate()
+        {
+            if (Speed < 20) { 
+                Speed += 5;
+            return Speed;
+            }
+            else
+            {
+                return Speed;
+            }
+        }
+
+        public int Brake()
+        { 
+            if(Speed > 0)
+            { 
+               Speed -= 5;
+            }
+            return Speed;
+        }
+
+        public int CurrentSpeed()
+        {
+            return Speed;
+        }
+    }
+}
---
+++ Assignment1OOPCarClass/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment1OOPCarClass;
+
+namespace carinfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car(2023, "Tesla", "S", 0);
+
+            Console.WriteLine("You are in your front garden.");
+            Console.WriteLine("Your car is in the garage.");
+            Console.WriteLine("You can end this by chosing E.");
+            Console.WriteLine("More speed, current speed, less speed (MCL)?.");
+
+            string chooseOption = "";
+            while (chooseOption != "e")
+            {
+                chooseOption = Console.ReadLine();
+                {
+                    switch (chooseOption)
+                    {
+                        case "e":
+                            Console.WriteLine("Bye Bye");
+                            break;
+                        case "m":
+                            if(myCar.Speed >= 20)
+                                Console.WriteLine("You cannot go faster than 20Km/h.");
+                            else 
+                                Console.WriteLine("Your current is speed is {0} km/h.", myCar.Accelerate());
+                                Console.WriteLine("More speed, current speed, less speed (MCL)?.");
+                            break;
+                        case "c":
+                            Console.WriteLine("Your current constant speed is {0} km/h.", myCar.Speed);
+                            Console.WriteLine("More speed, current speed, less speed (CL)?.");
+                            break;
+                        case "l":
+                            if (myCar.Speed <= 0)
+                                Console.WriteLine("Your are currently not moving at all.");
+                            else
+                                Console.WriteLine("Your current speed is {0} km/h.", myCar.Brake());
+                                Console.WriteLine("More speed, current speed, less speed (MCL)?.");
+                            break;
+                        default:
+                            Console.WriteLine("Not a valid option.");
+                            break;
+                    }
+
+
+                }
+
+
+            }
+
+        }
+    }
+}
